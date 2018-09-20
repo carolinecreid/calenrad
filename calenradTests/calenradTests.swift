cad04c4d69e12058583a48cab862f447878f8b5b
creidenberg <caroline@bashspree.com>
2018-09-20 11:23:33 -0600

    Initial Commit

---
+++ calenradTests/calenradTests.swift
@@ -0,0 +1,34 @@
+//
+//  calenradTests.swift
+//  calenradTests
+//
+//  Created by Caroline Creidenberg on 9/20/18.
+//  Copyright © 2018 Caroline Creidenberg. All rights reserved.
+//
+
+import XCTest
+@testable import calenrad
+
+class calenradTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
